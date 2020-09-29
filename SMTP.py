3162dd3b70f3cc2bf9f8af024f01c497cacc20ec
jonlop3z <36613298+jonlop3z@users.noreply.github.com>
2020-09-29 13:58:50 -0500

    Add files via upload

--- SMTP.py
+++ SMTP.py
@@ -4,6 +4,8 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
 from email.mime.text import MIMEText
 import smtplib
+from email.mime.base import MIMEBase
+from email import encoders
  
 # create message object instance
 msg = MIMEMultipart()
@@ -18,10 +20,22 @@ msg['From'] = sender
 msg['To'] =  recipients
 msg['Subject'] = "Correo de prueba"
 
+#Attach File
+part = MIMEBase('application', "octet-stream")
+part.set_payload(open("1.png", "rb").read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', 'attachment; filename="1.png"')
+msg.attach(part)
+
+part = MIMEBase('application', "octet-stream")
+part.set_payload(open("Adjunto.txt", "rb").read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', 'attachment; filename="Adjunto.txt"')
+msg.attach(part)
 # attach image to message body
-msg.attach(MIMEImage(open("1.png","rb").read()))
+#msg.attach(MIMEImage(open("1.png","rb").read()))
 msg.attach(MIMEText(message, 'plain')) 
- 
+
 # create server
 server = smtplib.SMTP('smtp.gmail.com: 587')
  
